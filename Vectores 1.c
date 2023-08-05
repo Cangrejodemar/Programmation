7bb3298bbcce8c65e5d3d3552df160064d526c3e
Cangrejodemar <141415103+Cangrejodemar@users.noreply.github.com>
2023-08-05 13:28:05 +0100

    Create Vectores 1.c

---
+++ Vectores 1.c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+
+    int main(){
+        
+        int vector [] = {1,2,3,5,9};
+        int i;
+        
+    for (i=0; i<5; i++){ // Cuando es cero o menor de 5 la variable i va incrementenado de posición se crea un contador de posiciones.
+    
+        
+        printf(" %i, ",vector[i]);// Al imprimir el vector con el contador "i" creado se obtine los valores del vector en orden ascendente.
+    
+        
+    }
+        
+    
+    
+}

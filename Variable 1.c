676f5e44e8a0a6fc5aed2364018180e735b2b247
Cangrejodemar <141415103+Cangrejodemar@users.noreply.github.com>
2023-08-05 09:25:08 +0100

    Update and rename Variable 1 to Variable 1.c

--- Variable 1.c
+++ Variable 1.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 
 int main(){
-int suma= 5+5;
-float pescado = 5.5;
-char letra = 'd';
-double pi = 3.1415926535;
+
+    int suma= 5+5;
+    float pescado = 5.5;
+    char letra = 'd';
+    double pi = 3.1415926535;
 
 
     printf(" Cinco mas cinco es %i ", suma);
@@ -13,7 +14,7 @@ double pi = 3.1415926535;
     printf("\n Mi letra preferida es la %c de dedo. ", letra); 
     printf("\n El número pi es %.16f ", pi);// Permite almacenar 8 bytes, un decimal con una precisión de 15 dígitos.
     
-     printf("\n\n Valor en bytes char: %u\n",sizeof(char)); 
+    printf("\n\n Valor en bytes char: %u\n",sizeof(char)); 
     printf(" Valor en bytes int: %u\n",sizeof(int)); 
     printf(" Valor en bytes float: %u\n",sizeof(float)); 
     printf(" Valor en bytes double: %u\n",sizeof(double)); 
